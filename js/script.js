17fe7aeb52f02cc8d341cdc29df087a131922e89
Azizul <azizulha429@gmail.com>
2024-04-10 18:15:47 +0600

    all done

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@ for (const btn of allBtn) {
   btn.addEventListener('click', function (e) {
     count = count - 1;
     selectSet = selectSet + 1;
-
+    if (selectSet > 4) {
+      alert('Must be needed 4 ticket')
+      return
+    }
     const seat = e.target.innerText;
     const seatPrice = document.getElementById('seat-price').innerText;
     const selectContainer = document.getElementById('tbody');
@@ -24,17 +27,13 @@ for (const btn of allBtn) {
     const selectSeatItem = document.getElementById('select-seat-total').innerText;
     const fiftyCoupon = document.getElementById('fifty').innerText;
     const twentyCoupon = document.getElementById('twenty').innerText;
-
+    const couponCode = document.getElementById('input-field').value;
     if (selectSeatItem == 3) {
-      document.getElementById('input-field').value = fiftyCoupon;
+      
       document.getElementById("apply-btn").removeAttribute('disabled')
       document.getElementById("apply-btn").style.backgroundColor = '#1DD100'
     }
-    if (selectSeatItem == 1) {
-      document.getElementById('input-field').value = twentyCoupon;
-      document.getElementById("apply-btn").removeAttribute('disabled')
-      document.getElementById("apply-btn").style.backgroundColor = '#1DD100'
-    }
+  
     const grandTotalString = document.getElementById('grand-total').innerText;
     const grandTotalCost = parseInt(grandTotalString);
     if (grandTotalCost >= 0) {
@@ -50,18 +49,23 @@ for (const btn of allBtn) {
 document.getElementById('apply-btn').addEventListener('click', function () {
   const inputField = document.getElementById('input-field');
   let couponValue = inputField.value;
+  const discountField = document.getElementById('discount-amount');
   const totalPrice = document.getElementById('total-amount');
   const productPriceElement = parseInt(totalPrice.innerText);
   if (couponValue === 'NEW15') {
     const discountPrice = parseFloat(productPriceElement * .15).toFixed(2);
+    discountField.innerText = discountPrice;
     const totalPriceSet = productPriceElement - discountPrice;
     document.getElementById('grand-total').innerText = totalPriceSet;
   } else if (couponValue === 'Couple20') {
     const discountPrice = parseFloat(productPriceElement * .20).toFixed(2);
+    discountField.innerText = discountPrice;
     const totalPriceSet = productPriceElement - discountPrice;
     document.getElementById('grand-total').innerText = totalPriceSet;
   } else {
     alert('Invalid Coupon')
   }
+  showElementById('discount')
+  hideElementById('apply-input')
 })
 
--- js/utility.js
+++ js/utility.js
@@ -9,5 +9,14 @@ function setInnerText(id, value) {
   document.getElementById(id).innerText = value;
 }
 
+function hideElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add('hidden');
+}
+function showElementById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove('hidden');
+}
+
 
 
